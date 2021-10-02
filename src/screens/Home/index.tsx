4afa64f3018ef678dc7e282883b18b0dd06c077f
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-02 20:18:08 -0300

    feat: Created Boilerplate ✨

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from './styles';
+
+export default function Home() {
+  return (
+    <S.Wrapper>
+      <S.Title>Hey!</S.Title>
+      <S.Subtitle>Let's get your order</S.Subtitle>
+    </S.Wrapper>
+  );
+}
