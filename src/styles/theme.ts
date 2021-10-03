f9cf6ac5622dd23b8a4e5ca12a8ec12652376806
GusttavoCastilho <gustavocvalenciano@gmail.com>
2021-10-03 11:26:42 -0300

    feat: add types from styled components and created theme ✨

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export default {
+    colors: {
+        black: '#000000',
+        gray: '#667C8A',
+        white: '#FFFFFF'
+    },
+    border: {
+        radius: '5px'
+    },
+    font: {
+        regular: 400,
+        medium: 500,
+        semiBold: 600,
+        bold: 700,
+    }
+} as const
